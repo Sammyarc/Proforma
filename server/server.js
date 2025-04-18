c987b557b475b7540477a9d0f3e3a502df177824
Sammyarc <nkemakolamsam@gmail.com>
2025-04-19 00:18:39 +0100

    Fixing cors

--- server/server.js
+++ server/server.js
@@ -17,17 +17,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
+  origin: allowedOrigins,
   credentials: true
 }));
 
