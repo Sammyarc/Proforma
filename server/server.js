862c26d2f16f028356ea36118904036acd43a79d
Sammyarc <nkemakolamsam@gmail.com>
2025-04-20 20:34:36 +0100

    Bug fixing on file size uploads

--- server/server.js
+++ server/server.js
@@ -25,8 +25,10 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
+
 
 app.options('*', cors()); // Enable pre-flight requests for all routes
 app.use("/api/auth", authRoutes);
