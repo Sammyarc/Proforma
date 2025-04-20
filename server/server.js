7b1159d687c85957264793d96c81639332f6e45d
Sammyarc <nkemakolamsam@gmail.com>
2025-04-20 20:26:17 +0100

    Added methods and allowedHeaders to the cors configuration

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
