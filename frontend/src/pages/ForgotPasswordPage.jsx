3719e5b81b483c7e294c0bb6dcc7df69f40fac52
Sammyarc <nkemakolamsam@gmail.com>
2025-04-26 18:07:57 +0100

    Added the forgot password and reset password page

---
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/authStore";
+import { ArrowLeft, Loader, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+import { GoMail } from "react-icons/go";
+
+const ForgotPasswordPage = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const { isLoading, forgotPassword } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await forgotPassword(email);
+    setIsSubmitted(true);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[90vw] md:w-[30vw] flex flex-col mx-auto my-[8vw] bg-White shadow-xl px-[2.5vw] py-[7vw] md:p-[2vw] rounded-[0.5vw]">
+        <h2 className="text-center font-satoshi text-[7vw] md:text-[2.5vw] font-semibold text-Gray900 mb-[1vw]">
+          Forgot Password
+        </h2>
+
+        {!isSubmitted ? (
+          <form onSubmit={handleSubmit}>
+            <p className="text-[4vw] text-center text-gray-700 font-satoshi mb-[1.5vw] md:text-[1vw]">
+              Enter your email address and we&apos;ll send you a link to reset
+              your password.
+            </p>
+            <div className="relative mt-[3vw] md:mt-[1vw]">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                autoComplete="on"
+                value={email}
+                className="w-full outline-none border bg-transparent px-[2.5vw] py-[3vw] md:px-[1vw] md:py-[0.8vw] rounded-[1.5vw] md:rounded-[0.5vw] font-satoshi pr-[9vw] md:pr-[3vw] hover:outline hover:outline-2 hover:outline-neutral-700 focus:outline focus:outline-2 focus:outline-neutral-700"
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={() => setEmail(email.toLowerCase())}
+              />
+              <div className="absolute inset-y-0 right-[3vw] md:right-[1vw] flex items-center">
+                <GoMail className="text-Gray600 md:text-[1.3vw]" />
+              </div>
+            </div>
+            <button
+              className={`mt-[4vw] w-full md:mt-[1.5vw] ${
+                isLoading ? "bg-gray-400 cursor-not-allowed " : "bg-cyan-800"
+              } text-white text-[4vw] font-satoshi h-[10vw] border border-neutral-400 rounded-xl box md:h-[2.5vw] md:text-[1vw]`}
+              type="submit"
+            >
+              {isLoading ? (
+                <div className="flex justify-center items-center gap-[1vw]">
+                  <Loader className="size-6 animate-spin" />
+                  <span className="text-[4vw] font-satoshi md:text-[1vw]">
+                    Sending...
+                  </span>
+                </div>
+              ) : (
+                "Send Reset Link"
+              )}
+            </button>
+          </form>
+        ) : (
+          <div className="text-center">
+            <div className="w-16 h-16 bg-green-500 rounded-full flex items-center justify-center mx-auto mb-4">
+              <Mail className="h-8 w-8 text-white" />
+            </div>
+            <p className="text-gray-300 font-satoshi mb-6">
+              If an account exists for {email}, you will receive a password
+              reset link shortly.
+            </p>
+          </div>
+        )}
+        <div className="mt-[5vw] flex justify-center md:mt-[1.5vw]">
+          <Link
+            to={"/signup"}
+            className="text-[4vw] font-satoshi text-gray-500 hover:underline flex items-center md:text-[1vw]"
+          >
+            <ArrowLeft className="h-4 w-4 mr-2" /> Back to Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
