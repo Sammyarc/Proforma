6b04684417d3d1d2fc461f7ac12cec3f5e2b6490
Sammyarc <nkemakolamsam@gmail.com>
2025-04-26 18:11:44 +0100

    Added routes to the endpoints of verify email, forgot password and reset password

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,4 +1,4 @@
-import { signup, login, logout, googleSignIn, checkAuth } from "../controllers/auth.controller.js";
+import { signup, login, logout, googleSignIn, checkAuth, verifyEmail, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -10,6 +10,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/google-signin", googleSignIn);
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 router.get("/check-auth", verifyToken , checkAuth);
 
 export default router
