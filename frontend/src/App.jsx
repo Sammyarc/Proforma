79ab360a93ff80e60e8d00ecf94836fe887d46b6
Sammyarc <nkemakolamsam@gmail.com>
2025-05-02 23:58:40 +0100

    style modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./hooks/ProtectedRoute";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
@@ -13,55 +12,50 @@ import ResetPasswordPage from "./pages/ResetPasswordPage";
 import Success from "./pages/Success";
 import Cancel from "./pages/Cancel";
 
-
 const App = () => {
-  
-    return (
-        <Router>
-            <Routes>
-                {/* Public Routes */}
-                <Route path="/" element={<Home />}/>
-                <Route path="/signup" element={<Signup />}/> 
-                {/* Protected dashboard Route */}
-                <Route path="/dashboard/*" element={ <ProtectedRoute> <Dashboard />  </ProtectedRoute>  }/> 
-                {/* Error Pages */}
-                <Route path="*" element={<Home />} />
-                <Route path='/verify-email' element={<EmailVerificationPage />} />
-                <Route
-					path='/forgot-password'
-					element={
-							<ForgotPasswordPage />
-					}
-				/>
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Protected dashboard Route */}
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <Dashboard />{" "}
+            </ProtectedRoute>
+          }
+        />
+        {/* Error Pages */}
+        <Route path="*" element={<Home />} />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
-				<Route
-					path='/reset-password/:token'
-					element={
-						<RedirectAuthenticatedUser>
-							<ResetPasswordPage />
-						</RedirectAuthenticatedUser>
-					}
-                />
-                <Route path="/payment-success" element={
-                    <Success />
-                }
-                />
+        <Route
+          path="/reset-password/:token"
+          element={
+            <RedirectAuthenticatedUser>
+              <ResetPasswordPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route path="/payment-success" element={<Success />} />
 
-                <Route path="/payment-error" element={
-                    <Cancel />
-                }
-                />
-            </Routes>
+        <Route path="/payment-error" element={<Cancel />} />
+      </Routes>
 
-            <ToastContainer
-            autoClose={5000}
-            hideProgressBar={false}
-            closeOnClick
-                theme="light"
-                style={{ zIndex: 99999 }}
-          />
-        </Router>
-    );
+      <ToastContainer
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        theme="light"
+        style={{ zIndex: 99999 }}
+      />
+    </Router>
+  );
 };
 
 export default App;
