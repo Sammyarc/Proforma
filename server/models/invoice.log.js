6c5f0a6c886c6d81be65fc50ff69bba740590a76
Sammyarc <nkemakolamsam@gmail.com>
2025-05-16 16:13:31 +0100

    Added new file to track the creation of new invoices

---
+++ server/models/invoice.log.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const InvoiceLogSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("InvoiceLog", InvoiceLogSchema);
